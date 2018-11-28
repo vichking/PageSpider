d39169ce8a51221cba2ec6751ef4208dc73eaa0d
victoria <vichking@gmail.com>
2018-11-28 17:44:33 +0200

    first git commit of my study python project

---
+++ page_spider.py
@@ -0,0 +1,19 @@
+import os
+import argparse
+
+def main(database: str, url_list_file: str):
+    print("We are going to work with " + database)
+    print("We are going to scan " + url_list_file)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-db", "--database", help="SQLite File Name")
+    parser.add_argument("-i", "--input", help="File containing URLs to read")
+    args = parser.parse_args()
+    database_file = args.database
+    input_file = args.input
+    main(database = database_file, url_list_file= input_file)
+
+
+
