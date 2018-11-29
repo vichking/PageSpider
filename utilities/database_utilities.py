a3b4a5b7935f9233fad55cd1f4c781cf48ae8373
victoria <vichking@gmail.com>
2018-11-29 10:19:58 +0200

    first git commit of my study python project

--- utilities/database_utilities.py
+++ utilities/database_utilities.py
@@ -1,7 +1,7 @@
-def create_database(database_page: str):
-    #TODO: generate the database
-    pass
-
 def save_words_to_database(database_path: str, words_list: list):
     #TODO: save the words to the database
+    pass
+
+def create_database(database_page: str):
+    #TODO: generate the database
     pass
