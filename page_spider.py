007b29f73c3bf4822ce84f5412463181c22f07c2
victoria <vichking@gmail.com>
2018-12-04 12:40:25 +0200

    reformatted code commit

--- page_spider.py
+++ page_spider.py
@@ -1,6 +1,7 @@
 import os
 import argparse
 
+
 def main(database: str, url_list_file: str):
     print("We are going to work with " + database)
     print("We are going to scan " + url_list_file)
@@ -13,7 +14,4 @@ if __name__ == "__main__":
     args = parser.parse_args()
     database_file = args.database
     input_file = args.input
-    main(database = database_file, url_list_file= input_file)
-
-
-
+    main(database=database_file, url_list_file=input_file)
--- utilities/database_utilities.py
+++ utilities/database_utilities.py
@@ -1,10 +1,8 @@
 def save_words_to_database(database_path: str, words_list: list):
-    #TODO: save the words to the database
+    # TODO: save the words to the database
     pass
+
 
 def create_database(database_page: str):
-    #TODO: generate the database
-    pass
-
-def lala():
+    # TODO: generate the database
     pass
